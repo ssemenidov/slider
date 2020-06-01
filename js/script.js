90ef4e199423833b83e1b10c1ac81709cf4bd1f2
ssemenidov <23sergey03@gmail.com>
2020-06-01 15:44:13 +0800

    slick

--- js/script.js
+++ js/script.js
@@ -1,47 +1,7 @@
-const slideList = document.querySelectorAll(".slide__item");
-const dotList = document.querySelectorAll(".dot");
-const btn_prev = document.querySelector(".carousel__prev");
-const btn_next = document.querySelector(".carousel__next");
-const slide = document.querySelector(".slide");
-let count = 2;
-let active = count;
-let size = slideList[0].clientWidth;
-slide.style.transform = "translateX(" + -1 * count * size + "px)";
-function checkend() {
-  if (count == -1) {
-    count = slideList.length - 1;
-    slide.style.transition = "0s";
-    move();
-  } else if (count == slideList.length) {
-    count = 0;
-    slide.style.transition = "0s";
-    move();
-  } else {
-    slide.style.transition = "0.5s";
-    move();
-  }
-}
-function move() {
-  slide.style.transform = "translateX(" + -1 * count * size + "px)";
-  dotList[active].classList.remove("dot--active");
-  active = count;
-  console.log(active);
-
-  dotList[active].classList.add("dot--active");
-}
-
-for (let i = 0; i < dotList.length; i++) {
-  dotList[i].addEventListener("click", () => {
-    count = i;
-    console.log(i);
-    checkend();
+$(document).ready(function () {
+  $(".carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
   });
-}
-btn_next.addEventListener("click", () => {
-  count++;
-  checkend();
-});
-btn_prev.addEventListener("click", () => {
-  count--;
-  checkend();
 });
